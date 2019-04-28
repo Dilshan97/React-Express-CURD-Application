fe3cc091cb8f915c343d704fad606e00065b24d2
Dilshan <dilshanramesh81@gamil.com>
2019-04-29 02:27:07 +0530

    three components created

---
+++ src/components/create.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+
+class create extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <p>create</p>
+            </div>
+         );
+    }
+}
+ 
+export default create;
---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+
+class index extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <p>index</p>
+            </div>
+         );
+    }
+}
+ 
+export default index;
